95a3af2cf660540dd8cdf8b3020212defda35360
Tihitna <tihitnasete58@gmail.com>
2024-07-07 21:41:09 +0300

    decimal

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import { useState } from 'react';
 import './App.css';
 function App() {
   const [number, changeNum]= useState("0");
-  const [number2, changeNum2]= useState("0");
+  const [number2, changeNum2]= useState("");
   const [operator, changeOperator]= useState("");
   const addNumber = (value)=>{
    if(value==="AC"){
     changeNum("0");
-    changeNum2("0");
+    changeNum2("");
     changeOperator("");
    }
    else{
     if(operator===""){
       if(number==="0"){
-        changeNum(value)
+        if(value==="."){
+          changeNum(number+value)
+        }else{
+          changeNum(value)
+        }
       }
       else{
         changeNum(number+value)
@@ -21,7 +25,11 @@ function App() {
     }
     else{
       if(number2==="0"){
-        changeNum2(value)
+        if(value==="."){
+          changeNum2("0"+value)
+        }else{
+          changeNum2(value)
+        }
       }
       else{
         changeNum2(number2+value)
@@ -29,35 +37,56 @@ function App() {
     }
    }
   }
-  const percent = ()=>{
-    changeNum(number/100)
-  }
+  
   const sign = ()=>{
     changeNum(number*-1)
   }
+  const toFixedNumber = (number, digits, base) => {
+    var pow = Math.pow(base || 10, digits);
+    return Math.round(number * pow) / pow;
+  };
+  const limitDecimal = (result) => {
+    const [integer, decimal]=result.toString().split(".");
+    if(!decimal || decimal.length <= 5){
+      return result.toString()
+    }
+    const cut = decimal.slice(0, 12);
+    return `${integer}.${cut}`
+  }
+  const percent = ()=>{
+    let res = parseFloat(number) / 100;
+    changeNum(res)
+  }
   const equal = ()=>{
+    let result;
     switch(operator){
       case "+":
-        changeNum(parseFloat(number)+parseFloat(number2));
+        result= toFixedNumber(parseFloat(number) + parseFloat(number2), 10);
+       
         break;
       case "-":
-        changeNum(number-number2);
+        result= parseFloat(number) - parseFloat(number2);
         break;
       case "x":
-        changeNum(number*number2);
+        result= parseFloat(number) * parseFloat(number2);
         break;
       case "/":
-        changeNum(number/number2);
+        result= parseFloat(number) / parseFloat(number2);
+        break;
+      case "":
+        result=number;
         break;
       
     }
-    changeNum2("0")
+    const limitedResult = limitDecimal(result);
+    changeNum(limitedResult)
+    changeNum2("")
     changeOperator("")
   }
   return (
     <div className="App">
       <div className='calculator'>
-        <div className='container'>{number2!=="0"? number2:number}</div>
+        <div className='container'>{number2!==""? number2:number}</div>
         <div className="row">
           <button onClick={(e)=>{addNumber(e.target.value)}} value="AC">AC</button>
           <button onClick={sign} value="+/-">+/-</button>
@@ -77,7 +106,7 @@ function App() {
           <button onClick={(e)=>{changeOperator(e.target.value)}} value="+">+</button>
           <button onClick={(e)=>{addNumber(e.target.value)}} value="0">0</button>
           <div></div>
-          <button>.</button>
+          <button onClick={(e)=>{addNumber(e.target.value)}} value=".">.</button>
           <button onClick={equal}>=</button>
         </div>
       </div>
